bed1ee26f750f0e61ee556930f3939560b3bf915
gonzagaa <iigust4vo@gmail.com>
2025-05-02 01:52:04 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+    showNavOnScroll()
+}
+
+function showNavOnScroll() {
+    if(scrollY > 0) {
+        document.querySelector("#navigation").classList.add("scroll")
+    } else {
+        document.querySelector("#navigation").classList.remove("scroll")
+    }
+}
+
+function showButtonWhatsappOnScroll() {
+    if(scrollY > 200) {
+        document.querySelector("#buttonWhatsapp").classList.add("show")
+    } else {
+        document.querySelector("#buttonWhatsapp").classList.remove("show")
+    }
+}
+
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+}
+
+
+const larguraDaTela = window.innerWidth
+
+if (larguraDaTela < 800) {
+  var swiper4 = new Swiper(".mySwiper4", {
+    cssMode: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      mousewheel: true,
+      keyboard: true,
+      loop: true,
+  });
+} else {
+  var swiper4 = new Swiper(".mySwiper4", {
+      slidesPerView: 3,
+      spaceBetween: 20,
+      loop: true,
+      grabCursor: true,
+      freeMode: false,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+}
